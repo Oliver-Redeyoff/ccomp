ab4729da69d97867e78c141fbb887fac4d896ba8
Oliver <oliver.redeyoff@gmail.com>
2021-12-11 19:56:07 +0000

    Now also puts tac in file

--- main.c
+++ main.c
@@ -242,6 +242,147 @@ void print_tac(BASIC_BLOCK* current_BB) {
 
 }
 
+void generate_tac_file(BASIC_BLOCK* current_BB) {
+
+  char tac_file_name[80];
+  strcpy(tac_file_name, "./testing/tac_results/");
+  strcat(tac_file_name, program_name);
+  strcat(tac_file_name, ".txt");
+
+  FILE* fp = fopen(tac_file_name, "w+");
+
+  char* operation_characters[12] = {"p", "+", "-", "*", "/", "%", "==", "!=", ">", ">=", "<", "<="};
+
+  while (1) {
+    TAC* current_TAC = current_BB->leader;
+
+    while (1) {
+      switch (current_TAC->type) {
+
+        case LABEL_TAC_TYPE: 
+          fprintf(fp, "label %s", current_TAC->v.tac_label.name->lexeme);
+          break;
+
+        case GOTO_TAC_TYPE: 
+          fprintf(fp, "goto %s", current_TAC->v.tac_label.name->lexeme);
+          break;
+
+        case BLOCK_START_TAC_TYPE:
+          fprintf(fp, "block %d start %s", current_TAC->v.tac_block_delimiter.block_type, current_TAC->v.tac_block_delimiter.name->lexeme, current_TAC->v.tac_block_delimiter.arity);
+          if (current_TAC->v.tac_block_delimiter.parent_block_name != NULL) {
+            fprintf(fp, " (%s)", current_TAC->v.tac_block_delimiter.parent_block_name->lexeme);
+          }
+          fprintf(fp, "\n");
+          break;
+
+        case BLOCK_END_TAC_TYPE:
+          fprintf(fp, "block %d end %s %d", current_TAC->v.tac_block_delimiter.block_type, current_TAC->v.tac_block_delimiter.name->lexeme, current_TAC->v.tac_block_delimiter.arity);
+          if (current_TAC->v.tac_block_delimiter.parent_block_name != NULL) {
+            fprintf(fp, " (%s)", current_TAC->v.tac_block_delimiter.parent_block_name->lexeme);
+          }
+          fprintf(fp, "\n");
+          break;
+
+        case FUNCTION_CALL_TAC_TYPE:
+          fprintf(fp, "call %s\n", current_TAC->v.tac_function_call.name->lexeme);
+          break;
+
+        case BUILTIN_CALL_TAC_TYPE:
+          fprintf(fp, "call builtin %d", current_TAC->v.tac_builtin_call.type);
+          if (current_TAC->v.tac_builtin_call.argument != NULL) {
+            fprintf(fp, ", %s\n", current_TAC->v.tac_builtin_call.argument->lexeme);
+          } else {
+            fprintf(fp, "\n");
+          }
+          break;
+
+        case IF_TAC_TYPE: {
+          TAC_IF tac_if = current_TAC->v.tac_if;
+          fprintf(fp, "if ");
+          if (tac_if.condition_result->type == IDENTIFIER) { fprintf(fp, "%s ", tac_if.condition_result->lexeme); }
+          else if (tac_if.condition_result->type == TEMPORARY_REG_IDENTIFIER) { fprintf(fp, "t%d ", tac_if.condition_result->value); }
+          else if (tac_if.condition_result->type == ARGUMENT_REG_IDENTIFIER) { fprintf(fp, "a%d ", tac_if.condition_result->value); }
+          else if (tac_if.condition_result->type == RETURN_REG_IDENTIFIER) { fprintf(fp, "v%d ", tac_if.condition_result->value); }
+          fprintf(fp, "%s\n", tac_if.jump_label->lexeme);
+          break;
+        }
+
+        case RETURN_TAC_TYPE:
+          fprintf(fp, "return\n");
+          break;
+
+        case EXIT_PROGRAM_TAC_TYPE:
+          fprintf(fp, "exit\n");
+          break;
+
+        case OPERATION_TAC_TYPE: {
+          TAC_OPERATION operation = current_TAC->v.tac_operation;
+          char* destination;
+          char* operation_char = operation_characters[operation.op];
+          char* source1;
+          char* source2;
+          
+          // print destination
+          if (operation.dest->type == IDENTIFIER) { fprintf(fp, "%s = ", operation.dest->lexeme); }
+          else if (operation.dest->type == TEMPORARY_REG_IDENTIFIER) { fprintf(fp, "t%d = ", operation.dest->value); }
+          else if (operation.dest->type == ARGUMENT_REG_IDENTIFIER) { fprintf(fp, "a%d = ", operation.dest->value); }
+          else if (operation.dest->type == RETURN_REG_IDENTIFIER) { fprintf(fp, "v%d = ", operation.dest->value); }
+
+          // print source 1
+          if (operation.src1->type == IDENTIFIER) { fprintf(fp, "%s", operation.src1->lexeme); }
+          else if (operation.src1->type == TEMPORARY_REG_IDENTIFIER) { fprintf(fp, "t%d", operation.src1->value); }
+          else if (operation.src1->type == ARGUMENT_REG_IDENTIFIER) { fprintf(fp, "a%d", operation.src1->value); }
+          else if (operation.src1->type == RETURN_REG_IDENTIFIER) { fprintf(fp, "v%d", operation.src1->value); }
+          else if (operation.src1->type == CONSTANT) { fprintf(fp, "%d", operation.src1->value); }
+
+          // print source 2
+          if (operation.src2 != NULL) {
+            fprintf(fp, " %s ", operation_char);
+            if (operation.src2->type == IDENTIFIER) { fprintf(fp, "%s", operation.src2->lexeme); }
+            else if (operation.src2->type == TEMPORARY_REG_IDENTIFIER) { fprintf(fp, "t%d", operation.src2->value); }
+            else if (operation.src2->type == RETURN_REG_IDENTIFIER) { fprintf(fp, "v%d", operation.src2->value); }
+            else if (operation.src2->type == CONSTANT) { fprintf(fp, "%d", operation.src2->value); }
+          }
+
+          if (operation.assignement_type == INT_DECLARATION_ASSIGNEMENT_TYPE) {
+            fprintf(fp, " (int variable declaration) ");
+          }
+          else if (operation.assignement_type == CLOSURE_DECLARATION_ASSIGNEMENT_TYPE) {
+            fprintf(fp, " (closure variable declaration) ");
+          }
+
+          fprintf(fp, "\n");
+
+          break;
+        }
+        
+        default:
+          fprintf(fp, "UNKNOWN TAC\n");
+          break;
+
+      }
+
+      // move onto next tac in basic block
+      if (current_TAC->next == NULL) {
+        break;
+      } else {
+        current_TAC = current_TAC->next;
+      }
+
+    }
+
+    // move onto next basic block
+    if (current_BB->next == NULL) {
+      break;
+    } else {
+      current_BB = current_BB->next;
+    }
+  }
+
+  return;
+
+}
+
 void print_mips(MIPS_PROGRAM* program) {
 
   printf("\n");
@@ -285,7 +426,7 @@ void print_mips(MIPS_PROGRAM* program) {
 void generate_asm(MIPS_PROGRAM* program) {
 
   char asm_file_name[80];
-  strcpy(asm_file_name, "./testing/results/");
+  strcpy(asm_file_name, "./testing/mips_results/");
   strcat(asm_file_name, program_name);
   strcat(asm_file_name, ".asm");
 
@@ -357,13 +498,13 @@ int main(int argc, char** argv) {
   VALUE* result = interpret(tree);
   
   BASIC_BLOCK* root_BB = generate_TAC(tree);
+  generate_tac_file(root_BB);
   //print_tac(root_BB);
   
   MIPS_PROGRAM* program = generate_MIPS(root_BB);
-  //print_mips(program);
-
   // Generate MIPS asm file
   generate_asm(program);
+  //print_mips(program);
 
 
   return 0;
